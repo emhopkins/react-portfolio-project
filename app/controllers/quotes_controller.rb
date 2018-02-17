66a20dca0159499d319141e20350b565cba013f5
emhopkins <epoletto@lstreetc.com>
2018-02-17 00:10:12 -0600

    testing with rails api, creating empty react-app

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,7 +4,12 @@ class QuotesController < ApplicationController
     q = params[:q]
 
     if q.blank?
-      render status: 400, json: { error: 'Expected parameter `q` '}
+      # render status: 400, json: { error: 'Expected parameter `q` '}
+      q = ""
+      render(
+        status: 200,
+        json: Quote.where(["text LIKE ?", "%#{q}%"]).limit(100)
+      )
     else
       render(
         status: 200,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
   scope '/api' do
-    get :quote, to: 'quotes#index'
+    get :quotes, to: 'quotes#index'
   end
 end
