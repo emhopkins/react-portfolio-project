858f93e4363763fe0cf1b55c6dd3046573d82f40
emhopkins <epoletto@lstreetc.com>
2018-02-27 14:01:24 -0600

    refactoring character new component to be stateless

--- client/src/containers/CharactersNew.js
+++ client/src/containers/CharactersNew.js
@@ -3,15 +3,6 @@ import { connect } from 'react-redux';
 import { addCharacter } from '../actions/characterActions';
  
 class CharactersNew extends Component {
- 
-
-  constructor() {
-    super();
- 
-    this.state = {
-      name: ''
-    };
-  }
 
   handleOnSubmit = event => {
     event.preventDefault();
@@ -19,24 +10,18 @@ class CharactersNew extends Component {
     const { addCharacter, history } = this.props;
     // Create the character with the Redux action
     history.push('/')
-    addCharacter(this.state);
+    addCharacter({ name: `${document.getElementById('newCharacterName').value}` });
 
     // redirect to /characters route
     history.push('/characters')
-  }
- 
-  handleOnChange = event => {
-    this.setState({
-      name: event.target.value
-    });
   }
  
   render(){
     return (
       <form style={{ marginTop: '16px' }} onSubmit={this.handleOnSubmit} >
         <input 
-          type="text" 
-          onChange={this.handleOnChange} 
+          id="newCharacterName"
+          type="text"  
           placeholder="Add a Character" />
         <input type="submit" value="Add Character" />
       </form>
