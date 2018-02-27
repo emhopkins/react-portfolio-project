bb3dd2351b0556fd29de7e1e38643d5f849e02cd
emhopkins <epoletto@lstreetc.com>
2018-02-26 22:34:41 -0600

    fix for characters show

--- client/src/containers/CharacterShow.js
+++ client/src/containers/CharacterShow.js
@@ -8,7 +8,7 @@ const CharacterShow = ({ character }) =>
   </div>
  
 const mapStateToProps = (state, ownProps) => {
-  const character = state.characters.data.find(character => character.id === ownProps.match.params.characterId)
+  const character = state.characters.data.find(character => character.id == ownProps.match.params.characterId)
  
   if (character) {
     return { character }
