812956f28d0e44b15431de0f5da0d6e369fc2543
emhopkins <epoletto@lstreetc.com>
2018-02-26 22:59:02 -0600

    adding search feature to quotes view

--- client/src/components/Quotes.js
+++ client/src/components/Quotes.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import QuoteLi from './QuoteLi'
+import React, { Component } from 'react';
+import QuoteLi from './QuoteLi';
 
-const Quotes = ({quotes}) => {
-  return (
-    <div>
-    	<h1>Quotes Page</h1>
-        	{quotes.map((quote, index) => {
-        		return <div key={index}><QuoteLi key={index} quote={quote} /> - {quote.character.name}</div> })}
-        	}
-    </div>
-  );
-};
+class Quotes extends Component {
+
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      quotes: props.quotes
+    };
+  }
+
+  textSearch = event => {
+    let query = event.target.value
+    this.setState({
+      quotes: this.props.quotes.filter(q => q.text.includes(query) )
+    });
+  }
+ 
+  render(){
+	  return (
+	    <div>
+	    	<h1>Quotes Page</h1>
+	    	<div><label>Search:</label><input type="text" onChange={this.textSearch} /></div>
+	        	{this.state.quotes.map((quote, index) => {
+	        		return <div key={index}><QuoteLi key={index} quote={quote} /> - {quote.character.name}</div> })}
+	    </div>
+	  );
+	};
+}
 
 export default Quotes;
--- client/src/reducers/quotes_reducer.js
+++ client/src/reducers/quotes_reducer.js
@@ -7,10 +7,6 @@ export default function quotesReducer(state = { loading: false, data: [] }, acti
     case 'FETCH_QUOTES':
       return {loading: false, data: action.payload};
     case 'UPVOTE_QUOTE':
-    	// console.log(state.data.map(quote => { if (quote.id === action.payload.id) { return quote = action.payload } else {return quote};}))
-    	// desbugger;
-    	// return {loading:false, data: state.data.concat(action.payload) }
-			// return {loading: false, data: [ ...state.data, action. payload ] }
 			return {loading:false, data: state.data.map(quote => 
 				{ if (quote.id === action.payload.id) 
 					{ return quote = action.payload 
